f1b8ed8de48e3c22ed3ed7482ebee0dc0496fdc5
Egorik <tarasovecegor1@gmail.com>
2023-10-30 00:03:12 +0300

    create laba 6

---
+++ lr6/script.js
@@ -0,0 +1,12 @@
+let sum = 0;
+function addNumber (number){
+    sum += number;
+}
+function showResult(){
+    alert("Сумма цифр: " + sum);
+}
+
+let myName = prompt("Как тебя зовут?", "");
+alert (`Привет ${myName} !`);
+
+
